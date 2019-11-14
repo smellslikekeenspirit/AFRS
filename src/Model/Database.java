509509f2cfe294bed0c47e7c082967b5a1b73dd1
smellslikekeenspirit <pdn3628@rit.edu>
2019-11-13 23:27:22 -0500

    added skeleton files for model

---
+++ src/Model/Database.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Database {
+
+    private TextFileReader textFileReader;
+    private TextFileWriter textFileWriter;
+    private ArrayList<Itinerary> lastFlightInfo;
+
+    public ArrayList<Itinerary> getLastFlightInfo() {
+        return lastFlightInfo;
+    }
+
+    public ArrayList<Reservation> getReservationInfo(){
+        return null;
+    }
+
+    public ArrayList<Airport> getAirportInfo(){
+        return null;
+    }
+
+    public boolean reserveFlight(){
+        return true;
+    }
+}
---
+++ src/Model/TextFileWriter.java
@@ -0,0 +1,17 @@
+package Model;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class TextFileWriter {
+
+    private File file;
+
+    public TextFileWriter(String filename){
+        this.file = new File(filename);
+    }
+
+    public void writeToTextFile(){
+
+    }
+}
