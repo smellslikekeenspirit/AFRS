d2411a626d71a0daba84dfaf184a066a91f283db
Alice Li <axl5934@g.rit.edu>
2019-11-16 15:52:01 -0500

    added data files and did partial parsing for airports

--- src/Model/Airport.java
+++ src/Model/Airport.java
@@ -1,41 +1,46 @@
 package Model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Airport {
 
     private String code;
-    private String weather;
-    private String temp;
+    private String cityName;
+    private LinkedList<String> weather;
+    private LinkedList<Integer> temp;
     private int delay;
+    private int connectionTime;
 
-    public Airport(String code, String weather, String temp, int delay){
+    public Airport(String code, String cityName){
         this.code = code;
-        this.weather = weather;
-        this.temp = temp;
-        this.delay = delay;
+        this.cityName = cityName;
+        weather = new LinkedList<>();
+        temp = new LinkedList<>();
+    }
+
+    public void addWeatherCondition(String weatherCondition){
+        weather.add(weatherCondition);
+    }
+
+    public String getCurrentWeather(){
+        String currentWeather = weather.pop();
+        addWeatherCondition(currentWeather);
+        return currentWeather;
+    }
+
+    public void addTemperatureCondition(int temperatureCondition){
+        temp.add(temperatureCondition);
+    }
+
+    public int getCurrentTemperature(){
+        int currentTemperature = temp.pop();
+        addTemperatureCondition(currentTemperature);
+        return currentTemperature;
     }
 
     public String getCode() {
         return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getWeather() {
-        return weather;
-    }
-
-    public void setWeather(String weather) {
-        this.weather = weather;
-    }
-
-    public String getTemp() {
-        return temp;
-    }
-
-    public void setTemp(String temp) {
-        this.temp = temp;
     }
 
     public int getDelay() {
@@ -45,5 +50,15 @@ public class Airport {
     public void setDelay(int delay) {
         this.delay = delay;
     }
+
+
+    public int getConnectionTime() {
+        return connectionTime;
+    }
+
+    public void setConnectionTime(int connectionTime) {
+        this.connectionTime = connectionTime;
+    }
+
 
 }
--- src/Model/Database.java
+++ src/Model/Database.java
@@ -1,12 +1,117 @@
 package Model;
 
-import java.util.ArrayList;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.util.*;
 
 public class Database {
 
     private TextFileReader textFileReader;
-    private TextFileWriter textFileWriter;
     private ArrayList<Itinerary> lastFlightInfo;
+
+    private static String CITIES_FILENAME = "cities.txt";
+    private static String DELAY_TIMES_FILENAME = "delay_times.txt";
+    private static String MINIMUM_CONNECTION_TIMES_FILENAME = "minimum_connection_times.txt";
+    private static String TTA_FLIGHTS_FILENAME = "TTA_flights.txt";
+    private static String WEATHER_TEMPERATURE_FILENAME = "weather_temp.txt";
+    private static String FILE_DELIMITER = ",";
+
+    /**
+     * maps the airport code to the airport
+     */
+    private Map<String, Airport> airports;
+
+    /**
+     * @throws Exception if setting up the database has failed
+     */
+    public Database() throws Exception{
+        registerAirports();
+    }
+
+    /**
+     * @throws IOException if parsing the cities file failed
+     */
+    private void registerAirports() throws Exception{
+        airports = new HashMap<>();
+
+        //first get the airport codes and their city names
+        textFileReader = new TextFileReader(CITIES_FILENAME);
+        for (String line: textFileReader.readTextFile()) {
+            String[] lineTokens = line.split(FILE_DELIMITER);
+            String airportCode = lineTokens[0];
+            String airportCityName = lineTokens[1];
+
+            Airport airport = new Airport(airportCode, airportCityName);
+            if (!airports.containsKey(airportCode)) {
+                airports.put(airportCode, airport);
+            } else {
+                String message = "Error: trying to register two airports with the same" +
+                        "airport code \"" + airportCode + "\".";
+                throw new Exception(message);
+            }
+        }
+
+        //set airport delay times
+        textFileReader = new TextFileReader(DELAY_TIMES_FILENAME);
+        for(String line: textFileReader.readTextFile()){
+            String[] lineTokens = line.split(FILE_DELIMITER);
+            String airportCode = lineTokens[0];
+            int delayTime = Integer.parseInt(lineTokens[1]);
+
+            if(airports.containsKey(airportCode)){
+                Airport airport = airports.get(airportCode);
+                airport.setDelay(delayTime);
+            }
+            else{
+                String message = "Error: Trying to set delay time of unknown airport " +
+                        "\"" + airportCode + "\".";
+                throw new Exception(message);
+            }
+        }
+
+        //set airport connection times
+        textFileReader = new TextFileReader(MINIMUM_CONNECTION_TIMES_FILENAME);
+        for(String line: textFileReader.readTextFile()){
+            String[] lineTokens = line.split(FILE_DELIMITER);
+            String airportCode = lineTokens[0];
+            int connectionTime = Integer.parseInt(lineTokens[1]);
+
+            if(airports.containsKey(airportCode)){
+                Airport airport = airports.get(airportCode);
+                airport.setConnectionTime(connectionTime);
+            }
+            else{
+                String message = "Error: Trying to set connection time of unknown airport " +
+                        "\"" + airportCode + "\".";
+                throw new Exception(message);
+            }
+        }
+
+        //set airport weather and temperature conditions
+        textFileReader = new TextFileReader(WEATHER_TEMPERATURE_FILENAME);
+        for(String line: textFileReader.readTextFile()){
+            String[] lineTokens = line.split(FILE_DELIMITER);
+            String airportCode = lineTokens[0];
+            Airport airport;
+            if(airports.containsKey(airportCode)){
+                airport = airports.get(airportCode);
+            }
+            else{
+                String message = "Error: Trying to set connection time of unknown airport " +
+                        "\"" + airportCode + "\".";
+                throw new Exception(message);
+            }
+
+            for(int i=1; i<lineTokens.length; i+=2){
+                String weather = lineTokens[i];
+                int temperature = Integer.parseInt(lineTokens[i+1]);
+
+                airport.addWeatherCondition(weather);
+                airport.addTemperatureCondition(temperature);
+            }
+        }
+    }
 
     public ArrayList<Itinerary> getLastFlightInfo() {
         return lastFlightInfo;
@@ -23,7 +128,7 @@ public class Database {
     public boolean reserveFlight(){
         return true;
     }
-    
+
     public boolean deleteReservation(){
         return true;
     }
