5882bc171879066909444d7222155c7bafb12de1
Ryan Kohn <ryankohn@ryans-mbp-4.wireless.rit.edu>
2019-12-01 20:49:42 -0500

    worked on getReservationInfo, commenting

--- src/Controller/Strategies/DeleteReservation.java
+++ src/Controller/Strategies/DeleteReservation.java
@@ -9,8 +9,9 @@ public class DeleteReservation implements IRequestHandlerStrategy {
     @Override
     public String handleRequest(String request, RequestHandler requestHandler) {
         requestHandler.setState(new NoPartialRequests());
-
+        // parameters are given in a comma-separated list
         String[] parameters = request.split(",");
+        // expecting keyword, passenger, origin, destination
         if(parameters.length != 4) {
             return ("error, invalid parameters");
         }
--- src/Controller/Strategies/ReserveFlight.java
+++ src/Controller/Strategies/ReserveFlight.java
@@ -9,7 +9,11 @@ public class ReserveFlight implements IRequestHandlerStrategy {
     @Override
     public String handleRequest(String request, RequestHandler requestHandler) {
         requestHandler.setState(new NoPartialRequests());
+
+        // parameters are given in a comma-separated list
         String[] parameters = request.split(",");
+
+        // expecting keyword, id, and passenger
         if(parameters.length != 3) {
             return ("error, invalid parameters");
         }
